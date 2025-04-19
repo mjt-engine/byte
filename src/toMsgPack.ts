a15701f52507da1c60b5cfbdc8fe8c6efb55d786
Matt Taylor <matt@matthewjosephtaylor.com>
2025-04-19 15:28:54 -0500

    force types on toMsgPack to fix node Buffer issue

--- src/toMsgPack.ts
+++ src/toMsgPack.ts
@@ -1,5 +1,5 @@
 import { Packr } from "msgpackr";
-export const toMsgPack = <T>(obj: T) => {
+export const toMsgPack = <T>(obj: T): Uint8Array<ArrayBufferLike> => {
   const packr = new Packr({ structuredClone: true });
   return new Uint8Array(packr.encode(obj));
 };
